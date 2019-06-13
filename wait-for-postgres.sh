952013b8d45e19003b936ca7e0844017d4a245f9
Ivo Calado <ivo.calado@ifal.edu.br>
2019-06-13 19:40:24 -0300

    Cria imagem docker do projeto
    Signed-off-by: Ivo Calado <ivo.calado@ifal.edu.br>

---
+++ wait-for-postgres.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# wait-for-postgres.sh
+
+set -e
+
+host="$1"
+shift
+cmd="$@"
+
+until PGPASSWORD="parsifal" psql -h "$host" -U "parsifal" -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres is up - executing command"
+
+exec $cmd
+
+
