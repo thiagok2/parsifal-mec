4ab9b9c908d2629c4f20ad4cd49978ba27fc67b0
Tyago Goulart <tyagogoulartn@gmail.com>
2019-07-19 11:16:32 -0300

    Merging migrations conflicts

---
+++ parsifal/reviews/migrations/0050_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('reviews', '0049_article_evaluation_finished'),
+        ('reviews', '0046_review_statistical_methods'),
+    ]
+
+    operations = [
+    ]
