a957444223343f6d7abf5a858a575dc68d760527
Thiago Oliveira <thiagok2@gmail.com>
2019-08-14 14:04:22 -0300

    internacionalization

--- parsifal/reviews/settings/forms.py
+++ parsifal/reviews/settings/forms.py
@@ -4,12 +4,13 @@ from django import forms
 
 from parsifal.reviews.models import Review
 
+from django.utils.translation import ugettext as _
 
 class ReviewSettingsForm(forms.ModelForm):
     name = forms.SlugField(
             widget=forms.TextInput(attrs={ 'class': 'form-control' }), 
             label='URL',
-            help_text='Apenas letras, números, _ou - são permitidos.',
+            help_text=_('Only letters, numbers, _or - are allowed.'),
             max_length=255)
 
     class Meta:
