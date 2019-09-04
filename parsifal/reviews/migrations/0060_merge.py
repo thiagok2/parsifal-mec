dbd1483128be1f735401b0533a4e5a06a3c4e819
Tyago Goulart <tyagogoulartn@gmail.com>
2019-09-03 23:56:16 -0300

    merging model's changes

---
+++ parsifal/reviews/migrations/0060_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('reviews', '0059_auto_20190903_2009'),
+        ('reviews', '0059_auto_20190902_1615'),
+    ]
+
+    operations = [
+    ]
