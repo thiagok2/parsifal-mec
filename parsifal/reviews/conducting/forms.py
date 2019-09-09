03d63bdcdd5a2ab0a5ca3c14d1180154d28baa3e
Thiago Oliveira <thiagok2@gmail.com>
2019-09-09 16:35:09 -0300

    add manuscrito - #787

--- parsifal/reviews/conducting/forms.py
+++ parsifal/reviews/conducting/forms.py
@@ -36,12 +36,11 @@ class SharedFolderForm(forms.ModelForm):
 
 class DocumentForm(forms.ModelForm):
     
-    entry_type = forms.ChoiceField(widget=forms.Select(attrs={ 'class': 'form-control change-type', 'style': 'width: 100%;' }), choices=Document.ENTRY_TYPES, label= _('Entry type'))
+    entry_type = forms.ChoiceField(widget=forms.Select(attrs={ 'class': 'form-control change-type generic', 'style': 'width: 100%;' }), choices=Document.ENTRY_TYPES, label= _('Entry type'))
     bibtexkey = forms.CharField(label=_('BibTeX key'), widget=forms.TextInput(attrs={ 'class': 'form-control', 'style': 'width: 20%;' }), max_length=50, required=False)
     title = forms.CharField(widget=forms.Textarea(attrs={ 'class': 'form-control article article-r book book-r booklet booklet-r inbook inbook-r incollection incollection-r inproceedings inproceedings-r manual manual-r mastersthesis mastersthesis-r misc phdthesis phdthesis-r proceedings proceedings-r techreport techreport-r unpublished unpublished-r', 'rows': '1' }), max_length=255, required=False, label= _('title'))
     author = forms.CharField(widget=forms.Textarea(attrs={ 'class': 'form-control article article-r book book-r booklet inbook inbook-r incollection incollection-r inproceedings inproceedings-r manual mastersthesis mastersthesis-r misc phdthesis phdthesis-r unpublished  unpublished-r', 'rows': '1' }), max_length=500, required=False, label= _('author'))
-    abstract = forms.CharField(widget=forms.Textarea(attrs={ 'class': 'form-control', 'rows': '3' }), max_length=4000, required=False, label= _('abstract'), help_text=_('Max. 4000 characters'))
-    keywords = forms.CharField(widget=forms.Textarea(attrs={ 'class': 'form-control', 'rows': '1' }), max_length=500, required=False, label= _('keywords'))    
+    
     year = forms.CharField(widget=forms.TextInput(attrs={ 'class': 'form-control article article-r book book-r booklet inbook inbook-r incollection incollection-r inproceedings inproceedings-r manual mastersthesis mastersthesis-r misc phdthesis phdthesis-r proceedings proceedings-r techreport techreport-r unpublished', 'style': 'width: 20%;' }), max_length=10, required=False, label= _('year'))
     month = forms.CharField(widget=forms.TextInput(attrs={ 'class': 'form-control article book booklet inbook incollection inproceedings manual mastersthesis misc phdthesis proceedings techreport unpublished', 'style': 'width: 20%;' }), max_length=30, required=False, label= _('month'))
 
@@ -63,12 +62,15 @@ class DocumentForm(forms.ModelForm):
     organization = forms.CharField(widget=forms.TextInput(attrs={ 'class': 'form-control inproceedings manual proceedings' }), max_length=255, required=False, label= _('organization'))
     school = forms.CharField(widget=forms.TextInput(attrs={ 'class': 'form-control mastersthesis mastersthesis-r phdthesis phdthesis-r' }), max_length=255, required=False, label= _('school'))
     series = forms.CharField(widget=forms.TextInput(attrs={ 'class': 'form-control book inbook incollection inproceedings proceedings' }), max_length=255, required=False, label= _('series'))
-    language = forms.CharField(widget=forms.TextInput(attrs={ 'class': 'form-control' }), max_length=255, required=False, label= _('language'))
-
-    coden = forms.CharField(widget=forms.TextInput(attrs={ 'class': 'form-control', 'style': 'width: 20%;' }), max_length=255, required=False, label= _('coden'))
-    doi = forms.CharField(label='DOI', widget=forms.TextInput(attrs={ 'class': 'form-control', 'style': 'width: 20%;' }), max_length=50, required=False)
-    isbn = forms.CharField(label='ISBN', widget=forms.TextInput(attrs={ 'class': 'form-control', 'style': 'width: 20%;' }), max_length=30, required=False)
-    issn = forms.CharField(label='ISSN', widget=forms.TextInput(attrs={ 'class': 'form-control', 'style': 'width: 20%;' }), max_length=30, required=False)
+    
+    abstract = forms.CharField(widget=forms.Textarea(attrs={ 'class': 'form-control generic', 'rows': '3' }), max_length=4000, required=False, label= _('abstract'), help_text=_('Max. 4000 characters'))
+    keywords = forms.CharField(widget=forms.Textarea(attrs={ 'class': 'form-control generic', 'rows': '1' }), max_length=500, required=False, label= _('keywords'))    
+    
+    language = forms.CharField(widget=forms.TextInput(attrs={ 'class': 'form-control generic' }), max_length=255, required=False, label= _('language'))
+    coden = forms.CharField(widget=forms.TextInput(attrs={ 'class': 'form-control generic', 'style': 'width: 20%;' }), max_length=255, required=False, label= _('coden'))
+    doi = forms.CharField(label='DOI', widget=forms.TextInput(attrs={ 'class': 'form-control generic', 'style': 'width: 20%;' }), max_length=50, required=False)
+    isbn = forms.CharField(label='ISBN', widget=forms.TextInput(attrs={ 'class': 'form-control generic', 'style': 'width: 20%;' }), max_length=30, required=False)
+    issn = forms.CharField(label='ISSN', widget=forms.TextInput(attrs={ 'class': 'form-control generic', 'style': 'width: 20%;' }), max_length=30, required=False)
 
     note = forms.CharField(widget=forms.TextInput(attrs={ 'class': 'form-control' }), max_length=255, required=False, label= _('note'))
 
