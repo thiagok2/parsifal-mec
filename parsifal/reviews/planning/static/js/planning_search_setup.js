88284e55b81ac982fbcf09beff29151d1140e612
Tyago Goulart <tyago.goulart@itec.al.gov.br>
2019-10-17 11:26:30 -0300

    Finishing meta analysis config tab

--- parsifal/reviews/planning/static/js/planning_search_setup.js
+++ parsifal/reviews/planning/static/js/planning_search_setup.js
@@ -10,6 +10,30 @@ $(function () {
         }
     });
 
+    $("#conclusion-model").change(function() {
+        value = $(this).val();
+
+        if (value === 'COHEN') {
+            $("#developmental_effects").prop('readonly', true)
+            $("#teacher_effects").prop('readonly', true)
+            $("#zone_desired_effects").prop('readonly', true)
+
+            $("#no_effect").prop('readonly', false)
+            $("#small_effect").prop('readonly', false)
+            $("#intermediate_effect").prop('readonly', false)
+            $("#large_effect").prop('readonly', false)
+        } else if (value === 'HATTIE') {
+            $("#developmental_effects").prop('readonly', false)
+            $("#teacher_effects").prop('readonly', false)
+            $("#zone_desired_effects").prop('readonly', false)
+
+            $("#no_effect").prop('readonly', true)
+            $("#small_effect").prop('readonly', true)
+            $("#intermediate_effect").prop('readonly', true)
+            $("#large_effect").prop('readonly', true)
+        }
+    });
+
     $("#btn-save-setup").click(function () {
       var btn = $(this);
       $.ajax({
