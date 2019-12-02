66f85e0d48a270bddd1170caa2131bc74872462d
Glauber Ferreira <glauber.ferreira@ifal.edu.br>
2019-12-02 16:41:20 -0300

    Fixing SQL command

--- docker-config/postgres/docker-entrypoint-initdb.d/create-extension.sql
+++ docker-config/postgres/docker-entrypoint-initdb.d/create-extension.sql
@@ -1,3 +1,3 @@
 /* Executado quando o container inicia (apenas se o diretório de dados do
 PostgreSQL estiver vazio) */
-CREATE IF NOT EXISTS EXTENSION pg_stat_statements;
+CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
